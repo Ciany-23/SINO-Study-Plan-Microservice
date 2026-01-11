1e6377603e82fd70e8abe47d6f28a8f7cf97f661
Ciany <cianyamadorsolis23@gmail.com>
2026-01-11 01:38:27 -0600

    Microservice of the study plan initiated

---
+++ src/main/java/com/studyplan/studyPlanMicroservice/StudyPlanMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.studyplan.studyPlanMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyPlanMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyPlanMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studyplan/studyPlanMicroservice/StudyPlanMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyplan.studyPlanMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyPlanMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
